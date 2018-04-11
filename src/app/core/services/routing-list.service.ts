09511de07d338e33fda705a887f62561b2d7fb95
blowedminds <ruzgardeniz.08@hotmail.com>
2018-04-11 23:57:28 +0300

    Some bugs fixed

--- src/app/core/services/routing-list.service.ts
+++ src/app/core/services/routing-list.service.ts
@@ -1,25 +1,29 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { Observable }     from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class RoutingListService {
   routes: any = {
-    chat: { url: 'discuss/',
-      'rooms': { url: 'rooms/'},
-      'room': { url: 'room/'},
-      'message': { url: 'message/'},
-      'messages': { url: 'messages/'}
+    chat: {
+      url: 'discuss/',
+      'rooms': { url: 'rooms/' },
+      'room': { url: 'room/' },
+      'message': { url: 'message/' },
+      'messages': { url: 'messages/' }
     },
-    auth: { url: 'auth/',
+    auth: {
+      url: 'auth/',
       'register': { url: 'register/' },
       'is-authenticated': { url: 'is-authenticated/' },
       'login': { url: 'login/' },
       'logout': { url: 'logout/' }
     },
-    article: { url: 'editor/',
-      'article': { url: 'article/',
+    article: {
+      url: 'editor/',
+      'article': {
+        url: 'article/',
         'content': { url: 'content/' },
         'restore': { url: 'restore/' },
         'force-delete': { url: 'force-delete/' },
@@ -28,27 +32,32 @@ export class RoutingListService {
       'articles': { url: 'articles/' },
       'trash': { url: 'trashed-articles/' },
     },
-    admin: { url: 'editor/panel/',
+    admin: {
+      url: 'editor/panel/',
       'languages': { url: 'languages/' },
       'categories': { url: 'categories/' },
       'menus': { url: 'menus/' }
     },
-    image: { url: 'image/',
+    image: {
+      url: 'image/',
       'image': { url: 'image/' },
       'thumb': { url: 'thumb/' },
       'images': { url: 'images/' },
-      'edit': { url: 'edit/'}
+      'edit': { url: 'edit/' }
     },
-    user: { url: 'user/',
+    user: {
+      url: 'user/',
       'info': { url: 'info/' },
       'menus': { url: 'menus/en/' },
-      'dashboard': { url: 'dashboard/'},
-      'management': { url: 'management/'},
-      'profile-image': { url: 'profile-image/'},
-      'profile': { url: 'profile/'}
+      'dashboard': { url: 'dashboard/' },
+      'management': { url: 'management/' },
+      'profile-image': { url: 'profile-image/' },
+      'profile': { url: 'profile/' }
     },
-    public: { url: '',
-      'image': { url: 'images/',
+    public: {
+      url: '',
+      'image': {
+        url: 'images/',
         'author': { url: 'author/' }
       }
     }
@@ -56,15 +65,14 @@ export class RoutingListService {
 
   constructor() { }
 
-  getUrl(key: string): string
-  {
-    let parsedKey = this.parseKey(key);
+  getUrl(key: string): string {
+    const parsedKey = this.parseKey(key);
 
     let route = this.routes;
 
-    let url: string = '';
+    let url = '';
 
-    for(let i = 0; i < parsedKey.length; i++) {
+    for (let i = 0; i < parsedKey.length; i++) {
 
       url += route[parsedKey[i]].url;
 
@@ -74,21 +82,19 @@ export class RoutingListService {
     return url;
   }
 
-  private parseKey(key: string): Array<string>
-  {
-    let parsedKey = [];
+  private parseKey(key: string): Array<string> {
+    const parsedKey = [];
 
-    for(let i = 0; i < key.length; i++) {
+    for (let i = 0; i < key.length; i++) {
 
-      let index = key.indexOf('.');
+      const index = key.indexOf('.');
 
-      if(index === -1) {
+      if (index === -1) {
 
         parsedKey.push(key);
 
         break;
-      }
-      else if(index != 0) {
+      } else if (index !== 0) {
 
         parsedKey.push(key.slice(0, index));
       }
